b0176ad09de6c04fc0b97669ad06544908a23705
geremiaslugodelgado-arch <geremiaslugodelgado@gmail.com>
2025-10-20 16:02:02 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("hola")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.LeftTriangle)
+})
+basic.forever(function () {
+	
+})
